85a95404f9997d8341fa0dd3bb034e986a36c95a
QingShou <15568820@qq.com>
2023-09-10 08:39:39 +0800

    修复

--- src/main/kotlin/taboolib/module/simple/listener/SXAttributeListener.kt
+++ src/main/kotlin/taboolib/module/simple/listener/SXAttributeListener.kt
@@ -26,6 +26,5 @@ object SXAttributeListener {
         attributeUpdateEvent.call()
         val sxAttributeData = SXAttribute.getApi().getLoreData(player, null, attributeUpdateEvent.playerLores)
         SXAttribute.getApi().setEntityAPIData(SXAttributeListener::class.java, player.uniqueId, sxAttributeData)
-        println("当前线程: " + Thread.currentThread().name)
     }
 }
