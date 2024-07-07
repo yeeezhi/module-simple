6bfc09445df02889c7b4e5f46d6b598f8f6ec86e
Yeezhi <15568820@qq.com>
2024-07-07 19:03:10 +0800

    update

--- src/main/kotlin/me/yeezhi/common/util/ItemUtil.kt
+++ src/main/kotlin/me/yeezhi/common/util/ItemUtil.kt
@@ -1,6 +1,16 @@
 package me.yeezhi.common.util
 
+import me.yeezhi.common.util.ItemUtil.getLore
+import me.yeezhi.common.util.ItemUtil.setLore
+import net.md_5.bungee.api.ChatColor
+import org.bukkit.Bukkit
+import org.bukkit.Material
+import org.bukkit.enchantments.Enchantment
 import org.bukkit.inventory.ItemStack
+import taboolib.module.configuration.Config
+import taboolib.module.configuration.Configuration
+import taboolib.platform.util.ItemBuilder
+import taboolib.platform.util.hasLore
 import taboolib.platform.util.isAir
 import java.util.*
 
@@ -8,6 +18,66 @@ import java.util.*
  * 物品工具
  */
 object ItemUtil {
+
+    @Config("config.yml")
+    lateinit var config: Configuration
+        private set
+
+    /**
+     * 获取物品Lore列表
+     *
+     * @return 物品Lore列表
+     */
+
+    fun ItemStack.getLore(): MutableList<String> {
+        return ItemBuilder(this).lore
+    }
+
+    /**
+     * 设置物品Lore列表
+     *
+     * @return 物品
+     */
+
+    fun ItemStack.setLore(list: List<String>): ItemStack {
+        if (this.isAir) {
+            return this
+        }
+        if (this.itemMeta == null) {
+            return this
+        }
+        val itemMeta = this.itemMeta
+        itemMeta.lore = list
+        this.setItemMeta(itemMeta)
+        return this
+    }
+
+
+    /**
+     * 物品添加附魔
+     *
+     * @param enchantment            附魔
+     * @param level                  附魔等级
+     * @param ignoreLevelRestriction 忽略等级限制
+     * @return 物品
+     */
+
+    fun ItemStack.addEnchant(
+        enchantment: Enchantment, level: Int, ignoreLevelRestriction: Boolean
+    ): ItemStack {
+        if (this.isAir) {
+            return this
+        }
+        if (this.itemMeta == null) {
+            return this
+        }
+        val itemMeta = this.itemMeta
+        itemMeta.addEnchant(enchantment, level, ignoreLevelRestriction)
+        this.setItemMeta(itemMeta)
+        return this
+    }
+
+
 
     /**
      * 合并物品数量
@@ -17,10 +87,10 @@ object ItemUtil {
      * @return 合并后的物品列表
      */
 
-    fun List<ItemStack>.arrange(): MutableList<ItemStack> {
+    fun  List<ItemStack>.arrange(): MutableList<ItemStack> {
         val list: MutableList<ItemStack> = ArrayList()
         this.forEach f1@{
-            if (it.isAir) {
+            if (it.isAir){
                 return@f1
             }
             val itemStack = it.clone()
